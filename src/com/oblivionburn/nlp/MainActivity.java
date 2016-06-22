f964cbd18fc8abe3a6d7b02c824405f43e769f56
Oblivionburn <donney.lane@gmail.com>
2016-06-21 19:52:34 -0500

    v1.6a

--- src/com/oblivionburn/nlp/MainActivity.java
+++ src/com/oblivionburn/nlp/MainActivity.java
@@ -57,7 +57,7 @@ public class MainActivity extends Activity implements OnItemSelectedListener
     @Override
     protected void onCreate(Bundle savedInstanceState) 
     {
-        super.onCreate(savedInstanceState);
+        super.onCreate(savedInstanceState);        
         setContentView(R.layout.activity_main);
 
         handler = new Handler();
